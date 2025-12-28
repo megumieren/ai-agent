64853249c909d7804bf8b5b805a07f80830be672
Ersami <2381087337@qq.com>
2025-12-28 15:13:59 +0800

    引入PGSQL

---
+++ ai-agent-backend/src/main/java/com/wjq/aiagentbackend/rag/LoveAppRagCloudAdvisorConfig.java
@@ -0,0 +1,32 @@
+package com.wjq.aiagentbackend.rag;
+
+import com.alibaba.cloud.ai.dashscope.api.DashScopeApi;
+import com.alibaba.cloud.ai.dashscope.rag.DashScopeDocumentRetriever;
+import com.alibaba.cloud.ai.dashscope.rag.DashScopeDocumentRetrieverOptions;
+import org.springframework.ai.chat.client.advisor.RetrievalAugmentationAdvisor;
+import org.springframework.ai.chat.client.advisor.api.Advisor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class LoveAppRagCloudAdvisorConfig {
+
+    @Value("${spring.ai.dashscope.api-key}")
+    private String dashscopeApiKey;
+
+    @Bean
+    public Advisor loveAppRagCloudAdvisor() {
+        DashScopeApi dashScopeApi = new DashScopeApi(dashscopeApiKey);
+        final String KNOWLEDGE = "恋爱大师";
+        //创建文档检索器
+        DashScopeDocumentRetriever dashScopeDocumentRetriever = new DashScopeDocumentRetriever(dashScopeApi,
+                DashScopeDocumentRetrieverOptions.builder()
+                        .withIndexName(KNOWLEDGE)
+                        .build());
+        //返回检索增强顾问对象
+        return RetrievalAugmentationAdvisor.builder()
+                .documentRetriever(dashScopeDocumentRetriever)
+                .build();
+    }
+}
